cb747b478c3db98706f27f1dffc014f3a4727f9c
Ujjwal <ujjwalsinghgaekwad@gmail.com>
2025-01-01 22:56:36 +0530

    Update Index Store

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,11 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from '../components/container/Container';
+import Logo from "./Logo";
 
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    Logo,
 };
