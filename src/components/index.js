c88eaea07fa8f787443bd43396af3fc45ec65e5e
Ujjwal <ujjwalsinghgaekwad@gmail.com>
2025-01-02 23:02:51 +0530

    Update index store

--- src/components/index.js
+++ src/components/index.js
@@ -2,10 +2,14 @@ import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
 import Container from '../components/container/Container';
 import Logo from "./Logo";
+import Button from "./Button";
+import Input from "./Input";
 
 export {
     Header,
     Footer,
     Container,
     Logo,
+    Button,
+    Input,
 };
