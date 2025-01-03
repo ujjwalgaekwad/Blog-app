73b38694b4a0ed87025d039ba8225305bebbb996
Ujjwal <ujjwalsinghgaekwad@gmail.com>
2025-01-04 01:13:43 +0530

    Add Signup

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import { Signup as SignupPage } from '../components'
+function Signup() {
+    return (
+        <div className='py-8'>
+            <SignupPage />
+        </div>
+    )
+}
+
+export default Signup
